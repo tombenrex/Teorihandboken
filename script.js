998f19b84141d32bb7054fc9ceec682fa76e64a5
Tom Larsson <tmlsn@hotmail.com>
2025-03-07 11:38:56 +0100

    major fix/update

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const backToTopButton = document.getElementById("backToTop");
 const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
-// Show floating buttons when user scrolls down 200px
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 200) {
     backToTopButton.style.display = "block";
@@ -10,16 +9,14 @@ window.onscroll = function () {
   } else {
     backToTopButton.style.display = "none";
     menuToggle.style.display = "none";
-    navMenu.style.display = "none"; // Hide menu when scrolling up
+    navMenu.style.display = "none";
   }
 };
 
-// Toggle Navigation Menu
 menuToggle.addEventListener("click", function () {
   navMenu.style.display = navMenu.style.display === "block" ? "none" : "block";
 });
 
-// Smooth scroll to top when "Back to Top" button is clicked
 backToTopButton.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
